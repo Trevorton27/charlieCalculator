31be577c37456dd19ba1cfc4ef722c32138ef6ea
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-05-14 19:33:54 +0800

    initial

---
+++ script.js
@@ -0,0 +1,31 @@
+
+const calculate = document.querySelector('form');
+calculate.addEventListener
+let num1;
+let num2;
+
+function getInputs(num1, num2) {
+
+}
+
+function performAdd(num1, num2) {
+
+}
+
+function performSubtract(num1, num2) {
+
+}
+
+function performMultiply(num1, num2) {
+
+}
+
+function performDivide(num1, num2) {
+
+}
+
+function displayOutcome(){
+
+}
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
