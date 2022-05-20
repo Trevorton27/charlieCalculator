37688a08dd2c9a30d3fcd09215feb1681fc5a1d1
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-05-20 18:21:58 +0800

    moving along

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
-const number = document.getElementById('display');
+let num1;
+let num2;
+let operator;
+
+const numbers = document.querySelectorAll('.number-button');
+numbers.forEach((number) => {
+       
+      number.addEventListener("click", (e) => {
+        const click1 = document.getElementById('buttonOne').value;
+        handleDisplay(click1);       
+      }); 
+           
+      //number.addEventListener("click", (e) => {
+        //const click2 = document.getElementById('buttonTwo').value;
+        //handleDisplay(click2);
+      //});
+});
 
 
-buttonOne.addEventlistener('click', () => displayNumbers());
-buttonTwo.addEventlistener('click', () => displayNumbers());
+function handleDisplay(digit){
+  const displayNumber = document.getElementById('display');
+  displayNumber.value = digit;
+
+  performCalculation(digit);
+}
+
+function performCalculation(num1) {
+
+}
+
+
+
+
+
+
+
+
+
+
+/*
+const operators = document.querySelectorAll('.operator-button');
+operators.forEach((operator) (e) => {
+
+});
+
+buttonTwo.addEventlistener("click", (e) => displayNumbers());
 plusButton.addEventlistener();
 equals.addEventlistener();
 minusButton.addEventlistener();
 
 
-function displayNumbers() {
- number.value = 1;
-}
-
+        if (operator) {
+          number.clicked === num1;
+        } else {
+          number.clicked === num2;
+        }
+*/
